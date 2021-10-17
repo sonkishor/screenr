fe5a817bf100f78ce96558b0e9d64cf650b59b6a
sonkishor <34811054+sonkishor@users.noreply.github.com>
2021-10-17 12:00:13 +0530

    now gets all the pages

--- main.py
+++ main.py
@@ -3,27 +3,44 @@ from bs4 import BeautifulSoup as bs
 from pprint import pprint
 from csv import writer
 
-url = "https://www.screener.in/screens/214283/companies-creating-new-high/"
-
+url = "https://www.screener.in/company/compare/00000062/?page=1"
 request = requests.get(url)
 soup = bs(request.content, 'html.parser')
 
-table = soup.find("table", class_ = "data-table text-nowrap striped")
-rows = table.find_all("tr")
+def scrape(page_no):
+    url = "https://www.screener.in/company/compare/00000062/?page={}".format(page_no)
 
-headings_raw = rows[0].find_all("th", {"scope" : "colgroup"})
-headings = []
+    request = requests.get(url)
+    soup = bs(request.content, 'html.parser')
 
-with open('Stonks.csv', 'a') as f_object:
-    write = writer(f_object)
-    for heading in headings_raw:
-        head = heading.text
-        headings.append(head.replace('\n','').strip().replace("  ", " "))
-    write.writerow(headings)
+    table = soup.find("table", class_ = "data-table text-nowrap striped")
+    rows = table.find_all("tr")
 
-    for row in rows:
-        data = []
-        raw_data = row.find_all("td")
-        for datas in raw_data:
-            data.append(datas.text.replace('\n','').strip())
-        write.writerow(data)  
+    headings_raw = rows[0].find_all("th", {"scope" : "colgroup"})
+    headings = []
+
+    with open('Stonks.csv', 'a') as f_object:
+        write = writer(f_object)
+        for heading in headings_raw:
+            head = heading.text
+            headings.append(head.replace('\n','').strip().replace("  ", " "))
+        write.writerow(headings)
+
+        for row in rows:
+            data = []
+            raw_data = row.find_all("td")
+            for datas in raw_data:
+                data.append(datas.text.replace('\n','').strip())
+            write.writerow(data)  
+ 
+page_parent = soup.find("div", class_ = "flex-row flex-gap-8 flex-space-between flex-align-center")   
+page_content = page_parent.find("div",  class_ = "sub").text
+
+page_str = str(page_content)
+
+page_count = page_str.strip().split(" ")
+
+print(page_count[-1])
+
+for i in range(1,int(page_count[-1])+1):
+    scrape(i)
